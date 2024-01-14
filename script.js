901f97e6e94838c5135333e1a78c6b81425e89b5
Abhishekgth <128219073+Abhishekgth@users.noreply.github.com>
2024-01-14 13:30:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 var dateDiffInDays = function (date1, date2) {
   //   write your code here
+  const oneDay = 24 * 60 * 60 * 1000; 
+  const fDate = new Date(date1);
+  const sDate = new Date(date2);
+
+  const diffInTime = sDate.getTime() - fDate.getTime();
+
+ 
+  const diffInDays = Math.round(diffInTime / oneDay);
+
+  return diffInDays;
 };
 
 // Do not change the code below.
